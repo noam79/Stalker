b21e11051df4c18df05b4f3767a1b17582200ef0
noam79 <noameilon8@gmail.com>
2022-01-20 15:37:24 +0200

    Added helper abstractions

---
+++ Server/ClientHandlers/ITcpClientHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace Server.ClientHandlers
+{
+    public interface ITcpClientHandler
+    {
+        public void HandleClient(TcpClient client);
+
+        public void Close();
+    }
+}
---
+++ Server/Listeners/ITcpListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace Server.Listeners
+{
+    public interface ITcpListener
+    {
+        public TcpClient Accept();
+
+        public void Close();
+    }
+}
